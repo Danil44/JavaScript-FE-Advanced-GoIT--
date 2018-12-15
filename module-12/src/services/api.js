426440e4e85186c398f552fcd8d6dd9497a90c33
danil <44bridgeprod@gmail.com>
2018-12-15 20:55:19 +0200

    add module-12 homework with localstorage problem

--- module-12/src/services/api.js
+++ module-12/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const API_KEY = "5c114c40864415f64af8aa59512d57e54425cd8c989f2";
 export const fetchURLPreview = userLink => {
   return axios
-    .get(`http://api.linkpreview.net/?key=${API_KEY}&q=https://${userLink}`)
+    .get(`https://api.linkpreview.net/?key=${API_KEY}&q=https://${userLink}`)
     .then(response => {
       const previewData = {
         title: response.data.title,
